1544e4dd3117ad1cc357d62d5b38cdaecf6deb97
todoggler <kentokuraCS@gmail.com>
2023-08-14 01:58:55 +0900

    build: Crashlytics にエラーレポートを送信するようにする

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:flutter/foundation.dart';
 import 'firebase_options.dart';
 import 'package:flutter/material.dart';
 import 'package:withtone/app.dart';
@@ -7,10 +9,21 @@ void main() async {
   // runApp 前に binding を呼び出す
   WidgetsFlutterBinding.ensureInitialized();
 
-  // firebase 設定
+  // firebase プロジェクト設定
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
 
+  // firebase crashlytics 有効化
+  // エラーのレポートを送信する
+  // https://firebase.google.com/docs/crashlytics/get-started?hl=ja&platform=flutter
+  FlutterError.onError = (errorDetails) {
+    FirebaseCrashlytics.instance.recordFlutterFatalError(errorDetails);
+  };
+  // Pass all uncaught asynchronous errors that aren't handled by the Flutter framework to Crashlytics
+  PlatformDispatcher.instance.onError = (error, stack) {
+    FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
+    return true;
+  };
   runApp(const App());
 }
