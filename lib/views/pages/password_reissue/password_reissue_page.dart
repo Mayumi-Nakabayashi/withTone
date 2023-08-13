2ab303ac6b424d0b25ded01880cc3644552a2465
todoggler <kentokuraCS@gmail.com>
2023-08-13 11:38:17 +0900

    feat: PasswordReissuePageの見た目をFigmaに合わせる

--- lib/views/pages/password_reissue/password_reissue_page.dart
+++ lib/views/pages/password_reissue/password_reissue_page.dart
@@ -21,27 +21,73 @@ class _PasswordReissuePageState extends State<PasswordReissuePage> {
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 20.0),
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
-              const Text('パスワードをお忘れですか？'),
-              const Text('登録時のメールアドレスを入力してください。'),
+              const SizedBox(height: 20), // 適当な余白
+              const SizedBox(
+                width: double.infinity,
+                child: Text(
+                  'パスワードをお忘れですか？',
+                  textAlign: TextAlign.start,
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+                ),
+              ),
+              const SizedBox(height: 20), // 適当な余白
+              const SizedBox(
+                width: double.infinity,
+                child: Text(
+                  '登録時のメールアドレスを入力してください。',
+                  textAlign: TextAlign.start,
+                ),
+              ),
+              const SizedBox(height: 40), // 適当な余白
+
+              const SizedBox(
+                width: double.infinity,
+                child: Text(
+                  'メールアドレス',
+                  textAlign: TextAlign.start,
+                  style: TextStyle(
+                    fontWeight: FontWeight.normal,
+                    fontSize: 12,
+                  ),
+                ),
+              ),
               TextFormField(
-                restorationId: 'life_story_field',
-                focusNode: FocusNode(),
+                cursorColor: Colors.black,
                 decoration: const InputDecoration(
                   border: OutlineInputBorder(),
                   hintText: "メールアドレスを入力してください",
-                  labelText: "メールアドレス",
                 ),
+                focusNode: FocusNode(),
+                maxLength: 40,
+                autovalidateMode: AutovalidateMode.onUserInteraction,
+                validator: (inputText) {
+                  // TODO(ケンティー): 各種バリデーションはちゃんと考えて定義する. 共通化して他の箇所でも利用したい.
+                  if (inputText == null || inputText == '') {
+                    return '入力してください';
+                  } else if (inputText.contains(' ')) {
+                    return 'スペースが含まれています';
+                  }
+                  return null;
+                },
               ),
+              const SizedBox(height: 20), // 適当な余白
+
               SizedBox(
                 width: double.infinity,
                 child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      fixedSize: const Size.fromWidth(double.maxFinite),
+                      backgroundColor: const Color(0xFFF73F96),
+                      foregroundColor: const Color(0xffffffff),
+                      surfaceTintColor: const Color(0xFFF73F96),
+                      elevation: 0,
+                    ),
                     onPressed: () => Navigator.pushNamed(
                           context,
                           PasswordReissueConfirmPage.path,
                         ),
-                    child: const Text('送信')),
+                    child: const Text('送信する')),
               ),
             ],
           ),
