2bb16f4640b175b111b4f8e618aa205fbc4b5742
yui <y129may9th@gmail.com>
2023-08-17 23:59:27 +0900

    feat: add AppBar

--- lib/views/pages/content/content_page.dart
+++ lib/views/pages/content/content_page.dart
@@ -15,22 +15,36 @@ class _ContentPageState extends State<ContentPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        color: Colors.red,
-        width: double.infinity,
-        height: double.infinity,
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              ElevatedButton(
-                onPressed: () => Navigator.pushNamed(context, ProfilePage.path),
-                child: const Text('profile ページに遷移'),
-              ),
-            ],
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        elevation: 0,
+        centerTitle: false,
+        title: const Text(
+          'チャレンジ',
+          style: TextStyle(
+            height: 24 / 18,
+            fontWeight: FontWeight.w800,
+            fontSize: 18,
           ),
         ),
+        actions: <Widget>[
+          // TODO: 検索機能み実装
+          IconButton(
+            icon: const Icon(
+              Icons.search,
+              size: 44,
+            ),
+            onPressed: () {},
+          ),
+          IconButton(
+            // FIXME: アイコン下手がきにしてます
+            icon: const Icon(
+              Icons.insert_emoticon_rounded,
+              size: 44,
+            ),
+            onPressed: () => Navigator.pushNamed(context, ProfilePage.path),
+          ),
+        ],
       ),
     );
   }
