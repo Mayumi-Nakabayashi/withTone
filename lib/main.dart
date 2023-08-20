49531bc86b8388d13301a12b81e2952b7e189b27
todoggler <kentokuraCS@gmail.com>
2023-08-20 19:07:23 +0900

    feat: Firebase 設定を main で読み込む

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:withtone/app.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+Future<void> main() async {
+  // runApp 前に　binding する
+  WidgetsFlutterBinding.ensureInitialized();
+
+  /// Firebase の設定を読み込む
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const App());
 }
