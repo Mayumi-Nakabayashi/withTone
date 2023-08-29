58e75d2e9b340d4f2c525219c827b8d56be598f0
sodateya <bssuketto@yahoo.co.jp>
2023-08-29 15:10:20 +0900

    feat:区切る線のウィジェット

---
+++ lib/views/components/border_widget.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+//設置とプライバシー画面のリストタイルのカテゴリを区切るためのウィジェット(上に線、その下にカテゴリ名)
+class BorderWidjet extends StatelessWidget {
+  const BorderWidjet({super.key, this.color = Colors.black38});
+
+  //borderの色デフォルトではColors.black38
+  final Color color;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        PreferredSize(
+            preferredSize: const Size.fromHeight(1),
+            child: Container(
+              height: 1, //高さ
+              color: Colors.black38, //色
+            )),
+      ],
+    );
+  }
+}
