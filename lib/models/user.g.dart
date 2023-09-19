dae533d4566dd731908e47d4bbe319f4134545d8
yuuya-1205 <64653733+yuuya-1205@users.noreply.github.com>
2023-09-19 22:18:18 +0900

    feat: freezed導入

---
+++ lib/models/user.g.dart
@@ -0,0 +1,25 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'user.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$_User _$$_UserFromJson(Map<String, dynamic> json) => _$_User(
+      userName: json['userName'] as String,
+      userId: json['userId'] as String? ?? '@withTone',
+      userImage: json['userImage'] as String? ?? '',
+      selfIntroduction: json['selfIntroduction'] as String? ?? '',
+      tiktokUrl: json['tiktokUrl'] as String? ?? '',
+      youtubeUrl: json['youtubeUrl'] as String? ?? '',
+    );
+
+Map<String, dynamic> _$$_UserToJson(_$_User instance) => <String, dynamic>{
+      'userName': instance.userName,
+      'userId': instance.userId,
+      'userImage': instance.userImage,
+      'selfIntroduction': instance.selfIntroduction,
+      'tiktokUrl': instance.tiktokUrl,
+      'youtubeUrl': instance.youtubeUrl,
+    };
