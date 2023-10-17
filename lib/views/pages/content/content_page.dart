1f33ed10145d3591a59e48b97233cbda30dc6321
yui <y129may9th@gmail.com>
2023-10-17 20:49:09 +0900

    feat: スクロール中にフローティングボタンを表示させない

--- lib/views/pages/content/content_page.dart
+++ lib/views/pages/content/content_page.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:withtone/views/components/admob/ad_banner.dart';
-import 'package:withtone/views/pages/content/content_appbar.dart';
 import 'package:withtone/views/pages/article/article_page.dart';
 import 'package:withtone/views/pages/content/article_card.dart';
+import 'package:withtone/views/pages/content/content_appbar.dart';
 import 'package:withtone/views/pages/content/learning_card.dart';
 import 'package:withtone/views/pages/profile/profile_page.dart';
 
@@ -17,120 +18,154 @@ class ContentPage extends StatefulWidget {
 }
 
 class _ContentPageState extends State<ContentPage> {
+  bool _visible = true;
+
   @override
   Widget build(BuildContext context) {
     final double floatingButtonWidth = MediaQuery.of(context).size.width * 0.75;
     const double floatingButtonHeight = 46;
+
+    final List<ArticleCard> articleItems = articleList
+        .map(
+          (item) => ArticleCard(
+            width: MediaQuery.of(context).size.width * 0.4,
+            imagePath: item['imagePath'],
+            title: item['title'],
+            timestamp: item['timestamp'],
+            onPressed: () => Navigator.pushNamed(context, ArticlePage.path),
+          ),
+        )
+        .toList();
+
+    final List<LeaningCard> leaningItems = learningList
+        .map(
+          (item) => LeaningCard(
+            thumbnail: item['thumbnail'],
+            title: item['title'],
+            timestamp: item['timestamp'],
+            subtitle: item['subtitle'],
+            tags: item['tags'],
+          ),
+        )
+        .toList();
+
     return Scaffold(
-      body: SafeArea(
-        child: Column(
-          children: [
-            const AdBanner(), //バナー広告
-            ContentAppBar(
-              title: const Text(
-                'チャレンジ',
-                style: TextStyle(
-                  height: 24 / 18,
-                  fontWeight: FontWeight.w800,
-                  fontSize: 18,
-                ),
-              ),
-              actions: [
-                IconButton(
-                  // FIXME: アイコン下手がきにしてます
-                  icon: const Icon(
-                    Icons.insert_emoticon_rounded,
-                    size: 44,
+        body: SafeArea(
+          child: Column(
+            children: [
+              const AdBanner(), //バナー広告
+              ContentAppBar(
+                title: const Text(
+                  'チャレンジ',
+                  style: TextStyle(
+                    height: 24 / 18,
+                    fontWeight: FontWeight.w800,
+                    fontSize: 18,
                   ),
-                  onPressed: () =>
-                      Navigator.pushNamed(context, ProfilePage.path),
                 ),
-              ],
-            ),
-            const SizedBox(
-              height: 26,
-            ),
-            // TODO: タブ切り替えの実装
-            const Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Text('新着メニュー'),
-                Text('記事'),
-                Text('イベント'),
-              ],
-            ),
-            SizedBox(
-              height: 195,
-              child: ListView(
-                scrollDirection: Axis.horizontal,
-                children: articleList
-                    .map(
-                      (item) => ArticleCard(
-                        width: MediaQuery.of(context).size.width * 0.4,
-                        imagePath: item['imagePath'],
-                        title: item['title'],
-                        timestamp: item['timestamp'],
-                        onPressed: () =>
-                            Navigator.pushNamed(context, ArticlePage.path),
-                      ),
-                    )
-                    .toList(),
+                actions: [
+                  IconButton(
+                    // FIXME: アイコン下手がきにしてます
+                    icon: const Icon(
+                      Icons.insert_emoticon_rounded,
+                      size: 44,
+                    ),
+                    onPressed: () =>
+                        Navigator.pushNamed(context, ProfilePage.path),
+                  ),
+                ],
               ),
-            ),
-            const Padding(
-              padding: EdgeInsets.only(top: 26.0),
-              child: Text(
-                '似たことで困っている質問を見てみよう',
-                style: TextStyle(
-                  height: 24 / 18,
-                  fontWeight: FontWeight.w800,
-                  fontSize: 18,
+              const SizedBox(
+                height: 26,
+              ),
+              // TODO: タブ切り替えの実装
+              const Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Text('新着メニュー'),
+                  Text('記事'),
+                  Text('イベント'),
+                ],
+              ),
+              SizedBox(
+                height: 195,
+                child: ListView(
+                  scrollDirection: Axis.horizontal,
+                  children: articleItems,
                 ),
               ),
-            ),
-            Expanded(
-              child: ListView(
-                children: learningList
-                    .map(
-                      (item) => LeaningCard(
-                        thumbnail: item['thumbnail'],
-                        title: item['title'],
-                        timestamp: item['timestamp'],
-                        subtitle: item['subtitle'],
-                        tags: item['tags'],
-                      ),
-                    )
-                    .toList(),
+              const Padding(
+                padding: EdgeInsets.only(top: 26.0),
+                child: Text(
+                  '似たことで困っている質問を見てみよう',
+                  style: TextStyle(
+                    height: 24 / 18,
+                    fontWeight: FontWeight.w800,
+                    fontSize: 18,
+                  ),
+                ),
               ),
-            ),
-          ],
-        ),
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-      floatingActionButton: SizedBox(
-        width: floatingButtonWidth,
-        height: floatingButtonHeight,
-        child: FloatingActionButton.extended(
-          onPressed: () {
-            // TODO: 遷移先の実装
-          },
-          label: const Text(
-            '今すぐ質問する',
-            style: TextStyle(
-              color: Colors.white,
-              fontWeight: FontWeight.bold,
-              fontSize: 16,
-            ),
+              Expanded(
+                child: NotificationListener<ScrollNotification>(
+                  onNotification: (notification) {
+                    /// スクロール中はボタンを非表示にする
+                    setState(() {
+                      _visible = false;
+                    });
+
+                    /// スクロールが止まった時にボタンを表示する
+                    ScrollDirection scrollDirection = ScrollDirection.idle;
+                    if (notification is UserScrollNotification) {
+                      if (notification.direction == scrollDirection) {
+                        setState(() {
+                          _visible = true;
+                        });
+                      }
+                    }
+                    return false;
+                  },
+                  child: ListView.builder(
+                    itemCount: leaningItems.length + 1,
+                    itemBuilder: (BuildContext context, int index) {
+                      if (index < leaningItems.length) {
+                        return leaningItems[index];
+                      } else {
+                        return const SizedBox(
+                            height: floatingButtonHeight + 20); // スペースの高さを調整
+                      }
+                    },
+                  ),
+                ),
+              ),
+            ],
           ),
-          backgroundColor: const Color(0xFF005792),
-          shape: const RoundedRectangleBorder(
-            borderRadius: BorderRadius.all(
-              Radius.circular(30),
-            ),
-          ),
         ),
-      ),
-    );
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+        floatingActionButton: _visible
+            ? SizedBox(
+                width: floatingButtonWidth,
+                height: floatingButtonHeight,
+                child: FloatingActionButton.extended(
+                  onPressed: () {
+                    // TODO: 遷移先の実装
+                  },
+                  label: const Text(
+                    '今すぐ質問する',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16,
+                    ),
+                  ),
+                  backgroundColor: const Color(0xFF005792),
+                  shape: const RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(30),
+                    ),
+                  ),
+                ),
+              )
+            : Container());
   }
 }
 
@@ -185,6 +220,27 @@ final List<Map<String, dynamic>> learningList = [
     'timestamp': 'October, 20 2021',
     'subtitle': '質問と回答をみる',
     'tags': ['violin'],
+  },
+  {
+    'thumbnail': 'assets/logo/appmain_color.png',
+    'title': 'ビブラートで挫折しないための練習',
+    'timestamp': 'October, 20 2021',
+    'subtitle': '質問と回答をみる',
+    'tags': [],
+  },
+  {
+    'thumbnail': 'assets/logo/appmain_color.png',
+    'title': 'ビブラートで挫折しないための練習',
+    'timestamp': 'October, 20 2021',
+    'subtitle': '質問と回答をみる',
+    'tags': [],
+  },
+  {
+    'thumbnail': 'assets/logo/appmain_color.png',
+    'title': 'ビブラートで挫折しないための練習',
+    'timestamp': 'October, 20 2021',
+    'subtitle': '質問と回答をみる',
+    'tags': [],
   },
   {
     'thumbnail': 'assets/logo/appmain_color.png',
