469fe18e671749ec3fa13260fac06c72efbb8171
yuuya-1205 <64653733+yuuya-1205@users.noreply.github.com>
2023-10-18 10:53:41 +0900

    feat: register_user_page作成

---
+++ lib/views/pages/edit_profile/register_user_page.dart
@@ -0,0 +1,85 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:withtone/models/user/register.dart';
+import 'package:withtone/providers/repositories/user_repository/user_repository.dart';
+import 'package:withtone/views/components/primary_button.dart';
+
+class ResisterUserPage extends StatefulWidget {
+  const ResisterUserPage({super.key});
+
+  static const String path = '/register_user_page';
+
+  @override
+  State<ResisterUserPage> createState() => _ResisterUserPageState();
+}
+
+class _ResisterUserPageState extends State<ResisterUserPage> {
+  @override
+  Widget build(BuildContext context) {
+    final userNameController = TextEditingController();
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.all(24.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const SizedBox(
+                  height: 252,
+                ),
+                const Text(
+                  '名前を作成',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(
+                  height: 2,
+                ),
+                const Text(
+                  'あとでいつでも変更できます。',
+                  style: TextStyle(
+                    fontSize: 12,
+                  ),
+                ),
+                const SizedBox(
+                  height: 8,
+                ),
+                TextFormField(
+                  controller: userNameController,
+                  cursorColor: Colors.black,
+                  decoration: const InputDecoration(
+                    hintText: "名前を入力してください",
+                  ),
+                  focusNode: FocusNode(),
+                  maxLength: 40,
+                  autovalidateMode: AutovalidateMode.onUserInteraction,
+                  validator: (inputText) {
+                    // TODO(ケンティー): 各種バリデーションはちゃんと考えて定義する. 共通化して他の箇所でも利用したい.
+                    if (inputText == null || inputText == '') {
+                      return '入力してください';
+                    }
+                    return null;
+                  },
+                ),
+                Center(
+                  child: PrimaryButton(
+                    label: '登録',
+                    onPressed: () async {
+                      final uid = FirebaseAuth.instance.currentUser!.uid;
+                      final userName = userNameController.text;
+                      final register = Register(id: uid, userName: userName);
+                      await RegisterRepository.updateRegister(register);
+                    },
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
