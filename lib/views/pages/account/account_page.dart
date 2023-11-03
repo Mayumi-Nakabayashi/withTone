462014e8350653dc19909dafd1f977c31ed0b22e
todoggler <kentokuraCS@gmail.com>
2023-11-03 18:35:09 +0900

    feat: ログアウトボタン押下でログイン前の画面まで戻る

--- lib/views/pages/account/account_page.dart
+++ lib/views/pages/account/account_page.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:withtone/splash_page.dart';
 import 'package:withtone/views/components/border_widget.dart';
 import 'package:withtone/views/components/settings_tile.dart';
 
@@ -16,11 +17,16 @@ class AccountPage extends StatefulWidget {
 
 class _AccountPageState extends State<AccountPage> {
   // ログアウトボタンを押した時の処理
-  Function onPressedLogoutButton = () {
+  Function onPressedLogoutButton = (BuildContext context) {
     // ログアウト
     final auth = FirebaseAuth.instance;
     GoogleSignIn().signOut();
     auth.signOut();
+    // ログイン前の画面に戻る
+    Navigator.of(context).pushNamedAndRemoveUntil(
+      SplashPage.path,
+      (_) => false,
+    );
   };
 
   @override
@@ -36,7 +42,10 @@ class _AccountPageState extends State<AccountPage> {
           children: [
             SettingsTile(title: 'アカウント情報', onPressed: () {}),
             SettingsTile(title: 'パスワード', onPressed: () {}),
-            SettingsTile(title: 'ログアウト', onPressed: onPressedLogoutButton()),
+            SettingsTile(
+              title: 'ログアウト',
+              onPressed: () => onPressedLogoutButton(context),
+            ),
             SettingsTile(title: 'アカウントの削除', onPressed: () {}),
             const BorderWidget()
           ],
