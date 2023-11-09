86af6d736a7dcf5950e089068aef9e0bbc5f8dc5
todoggler <kentokuraCS@gmail.com>
2023-11-09 23:06:17 +0900

    feat: 一度だけLottieWidgetのボタンを削除し、表示後最初のフレームに戻らなくする

--- lib/views/components/lottee/single_shot_lottie.dart
+++ lib/views/components/lottee/single_shot_lottie.dart
@@ -3,10 +3,17 @@ import 'package:lottie/lottie.dart';
 
 /// Lottieファイルを一度だけ再生するWidget
 class SingleShotLottie extends StatefulWidget {
-  const SingleShotLottie({super.key, required this.asset});
+  const SingleShotLottie({
+    super.key,
+    required this.asset,
+    this.size = 320,
+  });
 
   /// lottieファイルのassetパス
   final String asset;
+
+  /// 表示サイズ
+  final double size;
 
   @override
   State<SingleShotLottie> createState() => _SingleShotLottieState();
@@ -30,29 +37,18 @@ class _SingleShotLottieState extends State<SingleShotLottie>
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        SizedBox(
-          width: 200,
-          height: 200,
-          child: Lottie.asset(
-            widget.asset,
-            controller: _controller,
-            onLoaded: (composition) {
-              _controller
-                ..duration = composition.duration
-                ..forward().then((value) => _controller.reset());
-            },
-          ),
-        ),
-        ElevatedButton(
-            onPressed: () {
-              setState(() {
-                _controller.forward().then((value) => _controller.reset());
-              });
-            },
-            child: const Text('もう一度みる'))
-      ],
+    return SizedBox(
+      width: widget.size,
+      height: widget.size,
+      child: Lottie.asset(
+        widget.asset,
+        controller: _controller,
+        onLoaded: (composition) {
+          _controller
+            ..duration = composition.duration
+            ..forward();
+        },
+      ),
     );
   }
 }
