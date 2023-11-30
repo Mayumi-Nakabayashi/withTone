647463ca85173d29d12775b61ce0498cc554492b
Mayumi-Nakabayashi <mayumifinance@gmail.com>
2023-12-01 00:13:29 +0900

    feat: ボタン設置

--- lib/views/pages/professional_portal/professional_portal_page.dart
+++ lib/views/pages/professional_portal/professional_portal_page.dart
@@ -19,38 +19,54 @@ class _ProfessionalPortalPageState extends State<ProfessionalPortalPage> {
       body: Stack(
           children: [
             NestedScrollView(
-          headerSliverBuilder: (
-          BuildContext context,
-          bool innerBoxIsScrolled) {
-            return <Widget>[
-              SliverAppBar(
-            expandedHeight: 350.0,
-            floating: false,
-            pinned: true,
-            flexibleSpace:
-    FlexibleSpaceBar(
-    centerTitle: true,title:
-     const Text(
-    "Professional portalページ",style:
-    TextStyle(color: Colors.white,fontSize: 16.0,)
-    ),
-    background: Image.asset(
-    'assets/pictures/professional_portal_1.jpg',
-    fit:
-    BoxFit.cover)
-    ),
-    ),
-    ];
-            },
-      body:
-      const Center(child:
-      Text(""
-          "Sample Text"
+              headerSliverBuilder: (BuildContext context,
+                  bool innerBoxIsScrolled) {
+                return <Widget>[
+                  SliverAppBar(
+                    expandedHeight: 350.0,
+                    floating: false,
+                    pinned: true,
+                    flexibleSpace:
+                    FlexibleSpaceBar(
+                        centerTitle:
+                        true, title:
+                    const Text(
+                        "Professional portalページ",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 16.0,)
+                    ),
+                        background: Image.asset(
+                            'assets/pictures/professional_portal_1.jpg',
+                            fit: BoxFit.cover
+                        )
+                    ),
+                  ),
+                ];
+              },
+              body: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    FloatingActionButton.extended(
+                      onPressed: () {
+                      // TODO: 遷移先の実装
+                    },
+                      label: const Text(
+                        '今すぐ質問する',
+                        style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16,
+                        ),
+                      ),
+                    ),
+                  ]
+                ),
+              ),
+            ),
+          ]
       ),
-      ),
-    ),
-  ]
-    ),
     );
   }
 }
