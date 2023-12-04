8c9c6cf6b44ced0c818983007a6ba43224f2d903
sodateya <bssuketto@yahoo.co.jp>
2023-12-04 11:00:54 +0900

    feat:DataTime=>TimeStamp作成(ken-tyさんが作成済。不要になったら削除)

---
+++ lib/models/created_at_field.dart
@@ -0,0 +1,36 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+//TODO ken-tyさんと被るかもしてれないからいらなくなったら削除
+
+class CreatedAtField implements JsonConverter<DateTime?, dynamic> {
+  const CreatedAtField();
+
+  @override
+  DateTime? fromJson(dynamic timestamp) {
+    timestamp as Timestamp?;
+    return timestamp?.toDate();
+  }
+
+  // nullの時は toJson 時 FieldValue.serverTimestamp() を返すことで、createdAt の挙動になる
+  @override
+  dynamic toJson(DateTime? dateTime) {
+    if (dateTime == null) return FieldValue.serverTimestamp();
+    return dateTime;
+  }
+}
+
+class UpdatedAtField implements JsonConverter<DateTime?, dynamic> {
+  const UpdatedAtField();
+
+  @override
+  DateTime? fromJson(dynamic timestamp) {
+    timestamp as Timestamp?;
+    return timestamp?.toDate();
+  }
+
+  @override
+  FieldValue toJson(DateTime? date) {
+    return FieldValue.serverTimestamp();
+  }
+}
