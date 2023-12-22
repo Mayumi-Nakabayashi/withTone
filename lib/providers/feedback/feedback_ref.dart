c7c1cefe460e10b614f74ab98a243630973aeb2e
sodateya <bssuketto@yahoo.co.jp>
2023-12-22 14:31:57 +0900

    feat:feedbackのコレクションリファレンスのprovider作成

---
+++ lib/providers/feedback/feedback_ref.dart
@@ -0,0 +1,22 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+import 'package:withtone/models/feedback/feedback.dart';
+import 'package:withtone/models/post.dart';
+
+part 'feedback_ref.g.dart';
+
+@riverpod
+class FeedbackRef extends _$FeedbackRef {
+  @override
+  CollectionReference<Feedback> build(Post post) {
+    final feedbackRef = FirebaseFirestore.instance
+        .collection('posts')
+        .doc(post.id)
+        .collection('feedback')
+        .withConverter<Feedback>(
+          fromFirestore: (snapshot, _) => Feedback.fromJson(snapshot.data()!),
+          toFirestore: (feedback, _) => feedback.toJson(),
+        );
+    return feedbackRef;
+  }
+}
